b8e5e69ce741b5630c3f1d7a9b614e60cd3d5ba0
Lisa Copeland <lisa.copeland@lido.io>
2025-10-13 20:39:51 -0700

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink, RouterLinkActive],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'mytailwindapp';
   year = new Date().getFullYear();
+
+toggleTheme() {
+  const root = document.documentElement; // <html>
+  const isDark = root.classList.toggle('dark');
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+}
 
 ngOnInit() {
   const saved = localStorage.getItem('theme');
-  if (saved === 'dark') document.documentElement.classList.add('dark');
+  if (saved) {
+    document.documentElement.classList.toggle('dark', saved === 'dark');
+  } else {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    document.documentElement.classList.toggle('dark', prefersDark);
+  }
 }
-
 }
