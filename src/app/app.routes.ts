a31d4ed0d1f5ccae961b683fe9c456f3e59adef9
Lisa Copeland <lisa.copeland@lido.io>
2025-10-15 08:57:41 -0700

    routing, eliminate contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 
@@ -9,6 +8,5 @@ export const routes: Routes = [
       { path: 'home', component: HomeComponent },
       { path: 'projects', component: ProjectsComponent },
       { path: 'about', component: AboutComponent },
-      { path: 'contact', component: ContactComponent },
       { path: '**', redirectTo: '' }
 ];
